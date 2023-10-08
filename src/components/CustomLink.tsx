31ee0b9070cfc6ab10666298fc51fb3f7eb2fb14
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-10-08 13:08:45 -0300

    fix: bug in the url is in contact and the button clicked is the home button

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -6,6 +6,7 @@ const CustomLink = ({ Icon, text, href, activeLink, onClick }: ILink) => {
     <Link
       href={"/" + href}
       onClick={onClick}
+      suppressHydrationWarning={true}
       className={`w-full h-40 flex row items-center gap-2 px-2.5 py-2 text-sm rounded-def ${activeLink} hover:bg-link-1 hover:text-gray-1 active:bg-link-2`}
     >
       {Icon} {text}
