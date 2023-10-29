ceb9791301ffda3242e346ebb396ebb36433306e
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-10-29 14:20:28 -0300

    style: remove dark: two styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ import Header from '@/components/Header'
 export default function NotFound() {
   return (
     <main className="relative m-auto flex w-full max-w-6xl flex-row justify-between px-8 pb-16 max-lg:min-h-0 max-lg:justify-center max-lg:pb-5">
-      <div className="centralize-background relative z-[-1] mt-12 flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <div className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" />
+      <div className="centralize-background relative z-[-1] mt-12 flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:bg-gradient-to-br before:from-transparent before:to-blue-700 before:to-transparent before:opacity-10 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:from-sky-900 after:via-[#0141ff] after:via-blue-200 after:opacity-40 after:blur-2xl after:content-[''] before:lg:h-[360px]">
+        <div className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert" />
       </div>
 
       <Header />
