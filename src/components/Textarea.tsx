0c7f0c269b45d25506db8448d020a4b062c9cc52
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-11-01 11:41:52 -0300

    styled: tailwind css code formatting

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,6 +1,6 @@
 import { InputProps } from '@/interfaces'
 
-const Textarea = ({ label, type, name, htmlFor, id }: InputProps) => {
+const Textarea = ({ label, name, htmlFor, id }: InputProps) => {
   return (
     <div className="relative mb-5">
       <label className="text-md select-none font-medium" htmlFor={htmlFor}>
@@ -12,7 +12,7 @@ const Textarea = ({ label, type, name, htmlFor, id }: InputProps) => {
         name={name}
         required={true}
         autoComplete="off"
-        className="mt-1.5 h-20 w-full resize-none rounded-def border border-solid border-gray-4 bg-transparent p-2.5 text-sm duration-300 focus:border-gray-1"
+        className="focus:border-gray-1 mt-1.5 h-20 w-full resize-none rounded-def border border-solid border-gray-4 bg-transparent p-2.5 text-sm duration-300"
       />
     </div>
   )
