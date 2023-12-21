a0c6e2c2fa3fe16b083d408d2ca22b7b74f04194
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2023-12-21 11:43:11 -0300

    style: margin adjustment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import CustomLink from '@/components/CustomLink'
 const NotFound = () => {
   return (
     <div className="flex h-full animate-up flex-col items-center gap-y-6">
-      <div className="flex flex-col items-center gap-y-5 pt-8 lg:pt-0">
+      <div className="m-auto flex flex-col items-center gap-y-5 pt-8 lg:pt-0">
         <Paragraphy
           text="The page you are trying to access does not exist."
           style="text-center"
