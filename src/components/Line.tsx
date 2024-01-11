98a869635b4f62db6abfb92b06959a37a917612f
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-01-11 13:29:24 -0300

    style: adjustment of outline in Firefox browser entries

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,5 +1,5 @@
 const Line = () => {
-  return <hr className="block h-[0.056rem] w-full bg-gray-200" />
+  return <hr className="block min-h-[0.050rem] w-full bg-gray-200" />
 }
 
 export default Line
--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = ({ name, placeholder }: InputProps) => {
       required={true}
       autoComplete="off"
       placeholder={placeholder}
-      className="h-20 w-full resize-none rounded-def border border-solid border-gray-200 bg-transparent p-2.5 text-sm duration-300 focus:border-gray-100 placeholder:focus:text-transparent"
+      className="h-20 w-full select-none resize-none rounded-def border border-solid border-gray-200 bg-transparent p-2.5 text-sm duration-300 focus:border-gray-100 placeholder:focus:text-transparent"
     />
   )
 }
