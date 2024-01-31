97bc42616974088c32dba72c5a2c932f3786692e
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-01-31 14:42:12 -0300

    style: adjust the style of the ad lines

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,7 @@ const Input = ({ type, name, placeholder }: InputProps) => {
       required={true}
       autoComplete="off"
       placeholder={placeholder}
-      className="h-10 w-full select-none rounded-def border border-solid border-gray-200 bg-transparent p-2.5 text-sm duration-300 placeholder:text-gray-100 focus:border-secondary placeholder:focus:text-transparent"
+      className="h-10 w-full select-none rounded-def border border-solid border-gray-200 bg-transparent p-2.5 text-sm duration-300 placeholder:text-gray-100 placeholder:focus:text-transparent"
     />
   )
 }
