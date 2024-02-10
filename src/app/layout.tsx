dc285a4940edfa433c00c31a7c954d629a15d4dd
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-02-09 22:19:09 -0300

    style: adjustment for layout break when the modal is opened

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,14 +26,16 @@ export default function RootLayout({ children }: PropsWithChildren) {
         lang="en"
         className={cn('font-kanit selection:bg-neutral-500/10', kanit.variable)}
       >
-        <body className="m-auto flex w-full max-w-6xl flex-row justify-center gap-x-10 overflow-x-hidden px-6 pb-5 pt-24 sm:px-8 lg:pb-10 lg:pt-28">
-          <ThemeProvider>
-            <div id="notification-root"></div>
+        <body className="px-6 pb-5 pt-24 sm:px-8 lg:pb-10 lg:pt-28">
+          <div className="mx-auto flex w-full max-w-6xl flex-row justify-center gap-x-10 overflow-hidden">
+            <ThemeProvider>
+              <div id="notification-root"></div>
 
-            <Header />
-            <HeaderMobile />
-            <main className="w-full lg:max-w-[50rem]">{children}</main>
-          </ThemeProvider>
+              <Header />
+              <HeaderMobile />
+              <main className="w-full lg:max-w-[50rem]">{children}</main>
+            </ThemeProvider>
+          </div>
         </body>
       </html>
     </Providers>
