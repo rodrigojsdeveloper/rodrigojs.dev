aa60b542072ae7978651efb3192a1f1188bc3fae
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-03-16 13:54:29 -0300

    refactor: change project images

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -5,13 +5,9 @@ import { translate } from '@/i18n'
 
 export const Project = ({ img, title, link }: ProjectProps) => {
   return (
-    <li className="relative list-none overflow-hidden rounded-2xl border border-solid border-border focus-within:ring-1 focus-within:ring-border ">
+    <li className="relative list-none overflow-hidden rounded-radius border border-solid border-border focus-within:ring-1 focus-within:ring-border ">
       <Link href={link} target="_blank">
-        <Image
-          src={img}
-          alt={`${translate('projects.alt')} ${title}`}
-          className="max-h-[24.373rem]"
-        />
+        <Image src={img} alt={`${translate('projects.alt')} ${title}`} />
         <div className="absolute bottom-2 left-3 z-[1] flex w-full">
           <h2 className="font-light text-zinc-50">{title}</h2>
         </div>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ import { StaticImageData } from 'next/image'
 import { ProjectProps } from '@/interfaces'
 import catholicCommunity from '@/assets/catholic-community.png'
 import mathematical from '@/assets/mathematical.png'
-import motorsShop from '@/assets/motors-shop.png'
+import todo from '@/assets/todo.png'
 import devFinder from '@/assets/devfinder.png'
 import geography from '@/assets/geography.png'
 import iweather from '@/assets/iweather.png'
@@ -17,23 +17,19 @@ const createProject = (
 
 export const projects: ProjectProps[] = [
   createProject(
-    motorsShop,
-    'Motors Shop',
-    'https://github.com/rodrigojsdeveloper/motors-shop',
-  ),
-  createProject(
     catholicCommunity,
     'Catholic Community',
     'https://catholiccommunity.social',
   ),
+  createProject(geography, 'Geography', 'https://geography-plus.vercel.app'),
+  createProject(todo, 'ToDo', 'https://todo-boost.vercel.app'),
+  createProject(devFinder, 'devFinder', 'https://devfinder-plus.vercel.app'),
   createProject(
     mathematical,
     'Mathematical',
     'https://mathematical.vercel.app',
   ),
-  createProject(geography, 'Geography', 'https://geography-plus.vercel.app'),
-  createProject(devFinder, 'devFinder', 'https://devfinder-plus.vercel.app'),
   createProject(iweather, 'iWeather', 'https://iweather-plus.vercel.app'),
 ]
 
-export const initialHomeProject: ProjectProps = projects[2]
+export const initialHomeProject: ProjectProps = projects[0]
